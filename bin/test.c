39d3fa61e8e8a5da25290081cab9d399b09ec5b4
FurryBasket9978 <pgillbama@icloud.com>
2026-06-12 22:12:26 -0500

    began work on userspace tty

--- bin/test.c
+++ bin/test.c
@@ -1,34 +1,95 @@
 #include <stdio.h>
+#include <string.h>
+#include <vector.h>
+#include <unistd.h>
+#include <errno.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <sys/mman.h>
-#include <sys/io.h>
 
-#include <gfx/gfx.h>
-#include <gfx/rendering.h>
-#include <gfx/8x8font.h>
+#define MAX_BUFFER_LENGTH 256
 
-int main(void) {
-    int32_t serial_fd = open("/dev/serial", O_WRONLY, 0); 
-    int32_t input_fd = open("/dev/input", O_RDONLY, 0);
+int check_builtins(int argc, char* argv[]) {
+    if (argc == 0)
+        return 1;
+
+    if (strcmp(argv[0], "cd") == 0) {
+        if (argc >= 2)
+            chdir(argv[1]);
+        else 
+            printf("'cd' requires an argument\n");
+
+        return 1;
+    } else if (strcmp(argv[0], "exit") == 0) {
+        if (argc >= 2)
+            exit(atoi(argv[1]));
+        else
+            exit(0);
+    }
+
+    return 0;
+}
+
+int main() {
+    char buffer[MAX_BUFFER_LENGTH];
+    char cwd[MAX_BUFFER_LENGTH];
     
     while (1) {
-        uint8_t* buffer = malloc(10*sizeof(input_event_t));
-        uint32_t bytes_read = read(input_fd, buffer, 10*sizeof(input_event_t));
+        char* user_argv[10] = { 0 };
+        errno = 0; 
+        int ret = getcwd(cwd, MAX_BUFFER_LENGTH);
+        if (ret == -1) {
+            perror("ret");
+        }
 
-        input_event_t* events = (input_event_t*)buffer;
+        printf("%s >> ", cwd);
+        cwd[0] = '\0';
         
-        write(serial_fd, &events[0].ch, 1);
+        int32_t bytes = read(stdin, buffer, MAX_BUFFER_LENGTH - 1); 
+
+        char* token = strtok(buffer, ' ');
+        int user_argc = 0;
+        
+        while (token != NULL) {
+            user_argv[user_argc++] = token;
+            token = strtok(NULL, ' ');
+        }
+        
+        // if command is a builtin one then skip
+        if (check_builtins(user_argc, user_argv)) {
+            continue; 
+        }
+
+        // try to run the program, if not check the bin folder
+        char* path = malloc(MAX_BUFFER_LENGTH);
+        errno = 0;
+        int fd = open(user_argv[0], O_RDONLY, 0);
+        if (fd == -1 && errno == ENOENT) {
+            strcpy(path, "/bin/"); 
+            strcat(path, user_argv[0]);
+        } else if (fd == -1) {
+            perror("open");
+            free(path);
+            continue;
+        } else {
+            strcpy(path, user_argv[0]);
+        }
+
+        uint32_t child_pid = fork();
+        if (child_pid == 0) {
+            errno = 0; 
+             
+            int ret = execve(path, user_argv);
+            
+            if (ret == -1) {
+                perror("execve");
+                exit(-1);
+            }
+        }
+
+        int status;
+        waitpid(child_pid, &status, 0);
+        
+        free(path); 
     }
-    
-
-    gfx_context_t* ctx = gfx_get_context(NULL, NULL);
-    
-    uint32_t ret = gfx_draw_string(ctx, "WHATS UP\n", 100, 100, FB_WHITE, FB_BLACK);
-
-    gfx_flush(ctx);
-    
-    gfx_free_context(ctx); 
 
     return 0;
 }
--- bin/tty.c
+++ bin/tty.c
@@ -1,95 +1,41 @@
 #include <stdio.h>
-#include <string.h>
-#include <vector.h>
 #include <unistd.h>
-#include <errno.h>
 #include <stdlib.h>
+#include <gtx/gtx.c>
 
 #define MAX_BUFFER_LENGTH 256
 
-int check_builtins(int argc, char* argv[]) {
-    if (argc == 0)
-        return 1;
-
-    if (strcmp(argv[0], "cd") == 0) {
-        if (argc >= 2)
-            chdir(argv[1]);
-        else 
-            printf("'cd' requires an argument\n");
-
-        return 1;
-    } else if (strcmp(argv[0], "exit") == 0) {
-        if (argc >= 2)
-            exit(atoi(argv[1]));
-        else
-            exit(0);
-    }
-
-    return 0;
-}
-
 int main() {
-    char buffer[MAX_BUFFER_LENGTH];
-    char cwd[MAX_BUFFER_LENGTH];
-    
+    uint32_t input_fd = open("/dev/input", O_RDONLY, 0);
+    uint32_t ptm_fd = open("/dev/ptm", O_RDWR, 0);
+
+    gtx_context_t* ctx = gtx_get_context(NULL, NULL);
+
     while (1) {
-        char* user_argv[10] = { 0 };
-        errno = 0; 
-        int ret = getcwd(cwd, MAX_BUFFER_LENGTH);
-        if (ret == -1) {
-            perror("ret");
-        }
-
-        printf("%s >> ", cwd);
-        cwd[0] = '\0';
+        input_event_t* input_event_buffer = malloc(sizeof(input_event_t) * 10); 
+        uint32_t event_bytes_read = read(input_fd, input_event_buffer, sizeof(input_event_t) * 10);
+        uint32_t events_read = event_bytes_read / sizeof(input_event_t);
         
-        int32_t bytes = read(stdin, buffer, MAX_BUFFER_LENGTH - 1); 
+        uint8_t* bytes = malloc(10);
+        uint8_t num_chars = 0;
+        for (uint32_t i = 0; i < events_read; i++) {
+            input_event_t event = input_event_buffer[i];
 
-        char* token = strtok(buffer, ' ');
-        int user_argc = 0;
-        
-        while (token != NULL) {
-            user_argv[user_argc++] = token;
-            token = strtok(NULL, ' ');
-        }
-        
-        // if command is a builtin one then skip
-        if (check_builtins(user_argc, user_argv)) {
-            continue; 
-        }
-
-        // try to run the program, if not check the bin folder
-        char* path = malloc(MAX_BUFFER_LENGTH);
-        errno = 0;
-        int fd = open(user_argv[0], O_RDONLY, 0);
-        if (fd == -1 && errno == ENOENT) {
-            strcpy(path, "/bin/"); 
-            strcat(path, user_argv[0]);
-        } else if (fd == -1) {
-            perror("open");
-            free(path);
-            continue;
-        } else {
-            strcpy(path, user_argv[0]);
-        }
-
-        uint32_t child_pid = fork();
-        if (child_pid == 0) {
-            errno = 0; 
-             
-            int ret = execve(path, user_argv);
-            
-            if (ret == -1) {
-                perror("execve");
-                exit(-1);
+            if (event.ch) {
+                bytes[num_chars++] = event.ch; 
             }
         }
 
-        int status;
-        waitpid(child_pid, &status, 0);
+        uint32_t bytes_written = write(ptm_fd, bytes, num_chars);
         
-        free(path); 
+        uint32_t bytes_read = read(ptm_fd, bytes, 10);
+        bytes[bytes_read] = '\0';
+
+        gfx_draw_string(ctx, bytes, 100, 100, FB_WHITE, FB_BLACK);
+
+        free(bytes);
+        free(input_event_buffer);
     }
 
-    return 0;
+    get_free_context(ctx);
 }
